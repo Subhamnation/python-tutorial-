2dbe283d22464d613e14efe828fc64f933711eaa
Subham Chakraborty <89542026+Subhamnation@users.noreply.github.com>
2025-10-20 23:56:18 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+# Key points of __init__.py:
+# 1. Package Initialization: The __init__.py file is executed when the package is imported, allowing for package-level initialization.
+# 2. Namespace Definition: It defines the package namespace, making it possible to organize modules within the package.
+# 3. Module Imports: It can be used to import specific modules or functions, making them accessible directly from the package.
+# 4. Package Metadata: It can include metadata about the package, such as version information.
+# 5. Subpackage Inclusion: It can include subpackages, allowing for a hierarchical package structure
+
---
+++ test.py
@@ -0,0 +1,9 @@
+from package.math import *
+from package.subpackages.mul import multiply
+
+
+
+print(addition(10,5))
+print(subtraction(10,5))
+print(multiply(10,5))
+print(division(10,5))
